f5cc16283b77f8b90012fa4724459a522c316463
Cameron Lee <121077771+cameronlee101@users.noreply.github.com>
2024-01-22 19:16:43 -0800

    Footer (#7)
    * implemented footer
    
    * implemented cypress tests

---
+++ features/layout/footer/footer.tsx
@@ -0,0 +1,31 @@
+import styles from "./footer.module.scss";
+import Link from "next/link";
+import packageJson from "../../../package.json"; // Import package.json directly
+
+const footerLinks = [
+  { text: "Docs", href: "#" },
+  { text: "API", href: "#" },
+  { text: "Help", href: "#" },
+  { text: "Community", href: "#" },
+];
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.version}>
+        {"Version: " + packageJson.version || "Version unavailable"}
+      </div>
+      <div className={styles.links}>
+        {footerLinks.map((item) => (
+          <Link key={item.text} href={item.href}>
+            {item.text}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.logo}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={"/icons/logo-small.svg"} alt="logo" />
+      </div>
+    </footer>
+  );
+}
