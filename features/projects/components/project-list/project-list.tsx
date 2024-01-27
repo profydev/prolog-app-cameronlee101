367f79e67bdf3a9928d50759606572fb8fee8a85
Cameron Lee <cal26@sfu.ca>
2024-01-27 15:57:33 -0800

    completed error screen

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,7 +1,7 @@
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import styles from "./project-list.module.scss";
-import { LoadingIndicator } from "@features/ui";
+import { LoadingIndicator, ErrorAlert } from "@features/ui";
 
 export function ProjectList() {
   const { data, isLoading, isError, error } = useGetProjects();
@@ -12,7 +12,7 @@ export function ProjectList() {
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    return <ErrorAlert suffix="the project data" />;
   }
 
   return (
