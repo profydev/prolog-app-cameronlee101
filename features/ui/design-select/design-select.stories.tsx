531e6f89f5bc0282d29d1ee051de6c2a99f9b757
Cameron Lee <cal26@sfu.ca>
2024-02-06 11:16:05 -0800

    implemented basic styling for select element

---
+++ features/ui/design-select/design-select.stories.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import { DesignSelect } from "./design-select";
+
+export default {
+  title: "UI/DesignSelect",
+  component: DesignSelect,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as Meta<typeof DesignSelect>;
+
+const Template: StoryFn<typeof DesignSelect> = ({
+  placeholder,
+  options,
+  disabled,
+}) => (
+  <div style={{ padding: 10 }}>
+    <DesignSelect
+      placeholder={placeholder}
+      options={options}
+      disabled={disabled}
+    />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: "Select an option",
+  options: ["Option 1", "Option 2", "Option 3"],
+  disabled: false,
+};
+Default.parameters = {
+  viewMode: "docs",
+};
--- features/ui/design-select/design-select.tsx
+++ features/ui/design-select/design-select.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import styles from "./design-select.module.scss";
 
-export function DesignSelect() {
-  return <div>Enter</div>;
+type DesignSelectProps = {
+  placeholder?: string;
+  options?: string[];
+  disabled?: boolean;
+};
+
+export function DesignSelect({
+  placeholder = "Select an option",
+  options = ["Option 1", "Option 2", "Option 3"],
+  disabled = false,
+}: DesignSelectProps) {
+  return (
+    <select className={styles.select} disabled={disabled} required>
+      <option className={styles.option} value="" disabled selected hidden>
+        {placeholder}
+      </option>
+      {options.map((op, index) => (
+        <option className={styles.option} key={op + index} value={op}>
+          {op}
+        </option>
+      ))}
+    </select>
+  );
 }
