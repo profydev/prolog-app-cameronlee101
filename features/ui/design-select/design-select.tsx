4f22f0fced4ef0471fe156ef5d9eb64ce5875826
Cameron Lee <cal26@sfu.ca>
2024-02-06 20:36:45 -0800

    completed component states

--- features/ui/design-select/design-select.tsx
+++ features/ui/design-select/design-select.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./design-select.module.scss";
+import { Listbox } from "@headlessui/react";
+import classNames from "classnames";
 
 type DesignSelectProps = {
   placeholder?: string;
@@ -12,16 +14,46 @@ export function DesignSelect({
   options = ["Option 1", "Option 2", "Option 3"],
   disabled = false,
 }: DesignSelectProps) {
+  if (options.includes(placeholder)) {
+    throw new Error("Placeholder cannot be the same as any of the options.");
+  }
+
+  const [selectedOption, setSelectedOption] = useState<string>(placeholder);
+
   return (
-    <select className={styles.select} disabled={disabled} required>
-      <option className={styles.option} value="" disabled selected hidden>
-        {placeholder}
-      </option>
-      {options.map((op, index) => (
-        <option className={styles.option} key={op + index} value={op}>
-          {op}
-        </option>
-      ))}
-    </select>
+    <div className={styles.container}>
+      <Listbox value={selectedOption} onChange={setSelectedOption}>
+        <Listbox.Button
+          className={classNames(
+            styles.listboxButton,
+            disabled ? styles.disabled : "",
+            selectedOption == placeholder ? styles.placeholderSelected : "",
+          )}
+          aria-disabled={disabled}
+        >
+          {selectedOption}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/icons/chevron.svg" />
+        </Listbox.Button>
+        <Listbox.Options className={styles.listboxOptionsArea}>
+          {options.map((option, index) => (
+            <Listbox.Option key={index} value={option}>
+              {({ selected }) => (
+                <li
+                  className={classNames(
+                    styles.listboxOption,
+                    selected ? styles.selectedOption : "",
+                  )}
+                >
+                  {option}
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  {selected && <img src="/icons/check.svg" />}
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+    </div>
   );
 }
